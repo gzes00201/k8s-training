8965fb737a02ac6d690cd9f9d87e3e45563bc24e
gzes00201_wu <gzes00201_wu@mail.chungyo.net>
2022-05-07 23:42:47 +0800

    新增vm mysql

--- README.md
+++ README.md
@@ -5,9 +5,37 @@
 1. /front 裏面
 
 ## GKE 版
-1. [ping 連結](http://104.155.225.88/ping)
+1. [ping 連結](http://104.155.225.88:3000/ping)
 2. yaml在 gke資料夾內
 
 ## VM 版
 1. [ping 連結](http://34.81.41.241:3000/ping)
 2. 機器名稱 ami-test-vm
+  
+# K8s redis API 格式
+## POST /redis
+
+http://104.155.225.88:3000/redis  傳入body會將資料暫存在redis
+
+* 自定義key: 自定義資料
+
+## GET /redis/{key}
+
+http://104.155.225.88:3000/redis 取得暫存在redis的資料
+
+```json
+{
+    "msg": "",
+    "data": {
+        "redisKey": "redisValue"
+    }
+}
+```
+
+# VM版 mysql
+```docker
+$ docker run -itd --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=amiroot mysql:5.7.24
+$ docker run --name phpmyadmin -d --link mysql -e PMA_HOST="mysql" -p 8080:80 phpmyadmin/phpmyadmin
+$ docker start mysql
+$ docker start phpmyadmin
+```
--- front/src/server.js
+++ front/src/server.js
@@ -1,10 +1,55 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+const formData = require('express-form-data');
+const redis = require('redis');
+
 const app = express()
+
 const port = 3000
+
+const client = redis.createClient({
+  url: 'redis://:amitest@hello-ami-redis-service:6379'
+});
+client.on('error', (err) => console.log('Redis Client Error', err));
+client.on('connect', () => {
+  console.log('Redis client connected');
+});
+client.connect();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(formData.parse());
 
 app.get('/ping', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/redis/:rediskey', async (req, res) => {
+  console.log('rediskey', req.params.rediskey)
+  var value = await client.get(req.params.rediskey);
+  if(value){
+    res.send({msg: '', data: { [req.params.rediskey]: value  }})  
+  } else {
+    res.send({msg: '查無資料', data: { }})  
+  }
+ 
+})
+
+app.post('/redis', (req, res) => {
+  console.log(req.body);
+  var resData = {}
+  var joblist =  Object.keys(req.body).map(k=> client.set(k, req.body[k], redis.print))
+  
+  Promise.all(joblist).then(values => {
+    console.log(values); // [3, 1337, "foo"]
+    res.send({msg: '',data: resData})  
+  }).catch((err)=>{
+    console.log(err)
+    res.send({msg: '儲存失敗',data: {}})  
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
